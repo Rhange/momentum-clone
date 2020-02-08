1d8176b581f0efeb19a6bf2f2230196f817d5e8f
Rhange <rhange0511@gmail.com>
2020-02-08 14:00:04 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -13,7 +13,7 @@ function getWeather(lat, lon) {
     .then(function(json) {
       const temperature = json.main.temp,
         place = json.name;
-      weather.innerText = `${temperature} @ ${place}`;
+      weather.innerText = `${Math.round(temperature)}  @ ${place}`;
     });
 }
 
