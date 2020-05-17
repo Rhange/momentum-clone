2d0cf1d8ad3fc3000606c4dd622a96e88d1fc00b
Rhange <rhange0511@gmail.com>
2020-05-17 16:10:08 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -6,10 +6,10 @@ function getWeather(lat, lon) {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
   )
-    .then(function(response) {
+    .then(function (response) {
       return response.json();
     })
-    .then(function(json) {
+    .then(function (json) {
       const temperature = json.main.temp,
         place = json.name,
         country = json.sys.country,
